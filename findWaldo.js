b532bd5546f1c27b555244abe3bfe963f81f9caf
berkererol <berkererol@gmail.com>
2019-08-22 18:18:21 +0000

    Find Waldo with forEach method

--- findWaldo.js
+++ findWaldo.js
@@ -1,12 +1,11 @@
 // The second argument/parameter is expected to be a (callback) function
 const findWaldo = function(names, found) {
-  for (let i = 0; i < names.length; i++) {
-    let name = names[i];
-    if (name === "Waldo") {
-      found(i);   // execute callback
-
+  names.forEach(function (it, ind) {
+    if (it === "Waldo") {
+      found(ind)
     }
-  }
+  
+  })
 }
 
 const actionWhenFound = function(ind) {
